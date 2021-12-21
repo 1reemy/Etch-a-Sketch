6c295b64a0487cc0f59560151f93aa311271389b
Kareem Campbell <kareemd.campbell@gmail.com>
2021-12-20 21:37:20 -0500

    Flex box flex option added to allow for resizing of grids in square

--- grid.js
+++ grid.js
@@ -5,11 +5,18 @@ function arrange(gridRow,col){
     for(let i = 0; i <= (gridRow * col); i++){
         let row = document.createElement('div');
         let column = document.createElement('div');
-        row.style.cssText = "display:flex; width:10px; height:10px; border-style:solid";
-        column.style.cssText = "display:flex; width:10px; height:10px; border-style:solid";
+        row.style.cssText = "display:flex; flex: 1 1 0.5rem; background-color:white";
+        column.style.cssText = "display:flex; flex: 1 1 0.5rem; background-color:white";
 
         grid.appendChild(row);
         grid.appendChild(column);
+
+        row.addEventListener("mouseover",()=>{
+            row.style.setProperty("background-color", "red");
+        });
+        column.addEventListener("mouseover",()=>{
+            column.style.setProperty("background-color","red"); 
+        })
     };
 }
 arrange(16,16);
